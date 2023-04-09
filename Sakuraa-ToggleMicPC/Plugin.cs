3bb48c914af87a71cc0935ffaa2ff2616876a88a
HuddleX <73008150+HuddleX@users.noreply.github.com>
2023-04-08 20:25:27 -0700

    done
    done

--- Sakuraa-ToggleMicPC/Plugin.cs
+++ Sakuraa-ToggleMicPC/Plugin.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using BepInEx;
 using UnityEngine;
-using GorillaNetworking;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using Utilla;
 
 namespace Sakuraa_ToggleMicPC
@@ -10,45 +11,87 @@ namespace Sakuraa_ToggleMicPC
 	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
 	public class Plugin : BaseUnityPlugin
 	{
-		private GameObject GorillaComputer;
+        public Text text; // Text component to display the text on the screen
+        private bool ispttType = true; // Initial value is set to true
+        private GameObject GorillaComputer;
 
-		void Start()
+        void Start()
 		{
-			/* A lot of Gorilla Tag systems will not be set up when start is called /*
-			/* Put code in OnGameInitialized to avoid null references */
-
 			Utilla.Events.GameInitialized += OnGameInitialized;
 		}
 
 		void OnEnable()
 		{
-			/* Set up your mod here */
-			/* Code here runs at the start and whenever your mod is enabled*/
-
 			HarmonyPatches.ApplyHarmonyPatches();
 		}
 
 		void OnDisable()
 		{
-			/* Undo mod setup here */
-			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
-			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
-
 			HarmonyPatches.RemoveHarmonyPatches();
 		}
 
 		void OnGameInitialized(object sender, EventArgs e)
 		{
-			GorillaComputer = GameObject.Find(GorillaNetworking.GorillaComputer);
+            // Get the GorillaComputer GameObject by finding it in the scene
+            GorillaComputer = GameObject.Find("GorillaComputer");
 
-            // set the GorillaComputer.voiceChatOn to the string "YES"
+            // Check if GorillaComputer is found in the scene
+            if (GorillaComputer != null)
+            {
+                // Set pttType property to "TRUE"
+                GorillaComputer.GetComponent<GorillaNetworking.GorillaComputer>().pttType = "ALL CHAT";
+                Debug.Log("Push to talk: ALL CHAT");
+            }
+            else
+            {
+                Debug.LogWarning("Push to talk: GorillaComputer not found!");
+            }
 
 
+            GameObject canvas = new GameObject("Canvas");
+            Canvas canvasComponent = canvas.AddComponent<Canvas>();
+            canvasComponent.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.AddComponent<CanvasScaler>();
+            canvas.AddComponent<GraphicRaycaster>();
+            text = canvas.AddComponent<Text>();
+            text.text = "Mic active";
+            text.fontSize = 15;
+            text.color = Color.green;
+            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            text.alignment = TextAnchor.LowerRight;
         }
 
         void Update()
 		{
-			/* Code here runs every frame when the mod is enabled */
-		}
-	}
+            if (Keyboard.current.vKey.wasPressedThisFrame)
+            {
+                Debug.Log("V was pressed");
+                // Call the OnVoiceToggle method to toggle the pttType property
+                OnVoiceToggle();
+            }
+        }
+
+        void OnVoiceToggle()
+        {
+            ispttType = !ispttType;
+
+            // Update the pttType property of GorillaComputer based on the toggle value
+            if (GorillaComputer != null)
+            {
+                GorillaComputer.GetComponent<GorillaNetworking.GorillaComputer>().pttType = ispttType ? "ALL CHAT" : "PUSH TO TALK";
+                Debug.Log("Push to talk: " + (ispttType ? "ALL CHAT" : "PUSH TO TALK"));
+
+                if (GorillaComputer.GetComponent<GorillaNetworking.GorillaComputer>().pttType == "ALL CHAT")
+                {
+                    text.text = "Mic active";
+                    text.color = Color.green;
+                }
+                else
+                {
+                    text.text = "Mic Muted";
+                    text.color = Color.red;
+                }
+            }
+        }
+    }
 }
